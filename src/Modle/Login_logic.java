df799833ad275ff7c20bbe58a097f746a5d1e2e1
MGCAZPER <tharindumarasinghe56@gmail.com>
2026-02-07 20:46:20 +0530

    First Update

--- src/Modle/Login_logic.java
+++ src/Modle/Login_logic.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Modle;
 
-/**
- *
- * @author WW
- */
+package Modle;
+import Modle.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import View.Dashboard;
+
 public class Login_logic {
-    
+    public String Login(String username,String password){
+        if (username == null||username.trim().equals("") || password == null|| password.trim().equals("")){
+            return "Empty Feild";
+        }
+         String sql = "SELECT 1 FROM users WHERE username = ? AND password = ?";
+         try{
+            Connection con = (Connection) DBConnection.getInstance();
+            PreparedStatement pst = con.prepareStatement(sql);
+
+            pst.setString(1, username);
+            pst.setString(2, password);
+
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                
+               Dashboard dash=new Dashboard();
+                       dash.setVisible(true);
+               return "LS";
+            } else {
+                return "LF";
+            }
+         }catch(Exception e){
+             return "DE";
+         }
+    }
 }
